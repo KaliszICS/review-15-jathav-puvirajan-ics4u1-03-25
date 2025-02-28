8fef3f88dae71d4fbc9e8d246061aec01c4a3b3d
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-02-28 19:37:17 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,30 @@
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.println("\n\n");
+		goodString("pqr");
+		System.out.println("\n\n");
+	}
+	public static int goodString(String S){		
+		int minimumOperations = Integer.MAX_VALUE;
+		for(int i = 0; i < 26; i++){
+			char c = (char)(i + (int)'a');
+			int first = S.indexOf(c);
+			int last = S.lastIndexOf(c);
+			int cost = (first) + (S.length() - last - 1);
+			if(first == -1 && last == -1){
+				break;
+			}else if(cost <= minimumOperations){
+				minimumOperations = cost;
+			}
+		}
+		if(minimumOperations == Integer.MAX_VALUE){
+			System.out.print(minimumOperations);
+			minimumOperations = -1;
+		}
+		return minimumOperations;
 
 	}
-
-}
+}	
